f04f094e9c46a83c8b649d7fdd500d67fb4b248e
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-05-10 22:47:18 +0800

    feat: home layout

---
+++ src/constants/menu.ts
@@ -0,0 +1,18 @@
+export const menus = [
+  {
+    title: 'CSS样式',
+    path: '/css-style',
+    children: [
+      {
+        title: '渐变文字',
+        path: '/gradient-text',
+        description: '使用CSS渐变实现的文字效果'
+      },
+      {
+        title: '太极文字',
+        path: '/taichi-text',
+        description: '使用CSS渐变实现的太极文字效果'
+      },
+    ]
+  },
+]
