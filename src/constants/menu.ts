9dfabf080df51613f2c8111e09d2fa67d6bd04c6
Fog3211 <23151576+Fog3211@users.noreply.github.com>
2023-07-23 01:21:03 +0800

    feat: compute menu path

--- src/constants/menu.ts
+++ src/constants/menu.ts
@@ -1,16 +1,34 @@
-export const menus = [
+export type IMenuItem = {
+  title: string
+  path: string
+  key: string
+  description: string
+  children?: IMenuItem[]
+}
+
+export type IMenus = {
+  title: string
+  path: string
+  key: string
+  children: IMenuItem[]
+}
+
+export const menus: IMenus[] = [
   {
     title: 'CSS样式',
     path: '/css-style',
+    key: 'css-style',
     children: [
       {
         title: '渐变文字',
         path: '/gradient-text',
+        key: 'css-style__gradient-text',
         description: '使用CSS渐变实现的文字效果'
       },
       {
         title: '太极文字',
         path: '/taichi-text',
+        key: 'css-style__taichi-text',
         description: '使用CSS渐变实现的太极文字效果'
       },
     ]
---
+++ src/utils/common.ts
@@ -0,0 +1,16 @@
+import type { IMenuItem } from '../constants/menu'
+
+export function getMenuPath(menus: IMenuItem[], currentItem: IMenuItem, parentPath = ''): string {
+  let fullPath = parentPath
+
+  menus.forEach(menu => {
+    if (menu === currentItem) {
+      fullPath = `${parentPath}${currentItem.path}`;
+    } else {
+      if (menu.children) {
+        fullPath = getMenuPath(menu.children!, currentItem, fullPath + menu.path);
+      }
+    }
+  })
+  return fullPath
+}
